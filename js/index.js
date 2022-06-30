06026a2d94b1242c282def16ba387124269bec76
AnriNakamura <shironeko1009@icloud.com>
2022-07-01 01:02:48 +0900

    output the contents and the number of inputs

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 // receive input
+const taskList = [];    // list of tasks
+
 function output() {
-    out.innerText = 'Your input: ' + text.value;
+    taskList.push(newTask.value); // add task
+    out01.innerText = 'Your input: ' + taskList[taskList.length -1];
+    out02.innerText = 'Task number is ' + taskList.length;
 }
 
+const newTask = document.getElementById('newTask');
 const submit = document.getElementById('submit');
-const text = document.getElementById('text');
-const out = document.getElementById('out');
+const out01 = document.getElementById('out01');
+const out02 = document.getElementById('out02');
 
 submit.addEventListener('click', output);
