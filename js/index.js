9612b2a2992cd2eecb18cda8cc8ccf72c193b4d5
AnriNakamura <shironeko1009@icloud.com>
2022-07-01 01:16:34 +0900

    show all task

--- js/index.js
+++ js/index.js
@@ -1,15 +1,23 @@
-// receive input
-const taskList = [];    // list of tasks
+const taskList = [];                // list of tasks
 
-function output() {
-    taskList.push(newTask.value); // add task
+// add task and show info of task
+function addTask() {
+    taskList.push(newTask.value);   // add task
     out01.innerText = 'Your input: ' + taskList[taskList.length -1];
     out02.innerText = 'Task number is ' + taskList.length;
 }
 
+// show all task
+function show() {
+    out03.innerText = taskList;
+}
+
 const newTask = document.getElementById('newTask');
 const submit = document.getElementById('submit');
+const showList = document.getElementById('showList');
 const out01 = document.getElementById('out01');
 const out02 = document.getElementById('out02');
+const out03 = document.getElementById('out03');
 
-submit.addEventListener('click', output);
+submit.addEventListener('click', addTask);
+showList.addEventListener('click', show);
