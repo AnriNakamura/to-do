da7ab233f49d448660ee43335b90f2d4f146e6ce
AnriNakamura <shironeko1009@icloud.com>
2022-07-03 01:09:48 +0900

    together functions

--- js/index.js
+++ js/index.js
@@ -1,11 +1,5 @@
 const tasks = []; 
 
-// add task
-//const newTask= document.getElementById('newTask');
-//const taskList = document.getElementById('taskList');
-//const ul = document.createElement('ul');
-//const li = [];
-
 // create new div, input(checkbox) and label
 const newTask= document.getElementById('newTask');
 const taskList = document.getElementById('taskList');
@@ -18,24 +12,18 @@ function lastIndex(array){
     return array[array.length-1];
 }
 
-function hoge() {
+function addTask() {
     div.push(document.createElement('div'));
     input.push(document.createElement('input'));
     label.push(document.createElement('label'));
-}
 
-function hogeInfo() {
     lastIndex(input).type = 'checkbox';
     lastIndex(label).textContent = newTask.value;
-}
 
-function hogeAdd() {
     lastIndex(div).appendChild(lastIndex(input));
     lastIndex(div).appendChild(lastIndex(label));
     taskList.insertBefore(lastIndex(div), taskList.firstElementChild);
 }
 
 const submit = document.getElementById('submit');
-submit.addEventListener('click', hoge);
-submit.addEventListener('click', hogeInfo);
-submit.addEventListener('click', hogeAdd);
+submit.addEventListener('click', addTask);
