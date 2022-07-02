67feb59c4bc797639887eeda0a785b4e4d9d075a
AnriNakamura <shironeko1009@icloud.com>
2022-07-02 22:46:41 +0900

    create checkbox

--- js/index.js
+++ js/index.js
@@ -1,53 +1,41 @@
 const tasks = []; 
 
 // add task
-const input = document.getElementById('newTask');
-const div = document.getElementById('taskList');
-const ul = document.createElement('ul');
-const li = [];
+//const newTask= document.getElementById('newTask');
+//const taskList = document.getElementById('taskList');
+//const ul = document.createElement('ul');
+//const li = [];
 
-function addTask() {
-    li.push(document.createElement('li'));
-    li[li.length-1].textContent = newTask.value;
-    ul.insertBefore(li[li.length-1], ul.firstElementChild);
-    div.appendChild(ul);
-}
-
-const submit = document.getElementById('submit');
-submit.addEventListener('click', addTask);
-
-/*
-// add task and show info of task
-const newTask = document.getElementById('newTask');
-const taskName = document.createElement('p');
-const taskNum = document.createElement('p');
-const taskInfo = document.getElementById('taskInfo');
-
-function addTask() {
-    tasks.push(newTask.value);
-    taskName.textContent = 'Your input: ' + newTask.value;
-    taskNum.textContent = 'Task number is ' + tasks.length;
-    taskInfo.appendChild(taskName);
-    taskInfo.appendChild(taskNum);
-}
-
-// show all task
+// create new div, input(checkbox) and label
+const newTask= document.getElementById('newTask');
 const taskList = document.getElementById('taskList');
-const ul = document.createElement('ul');
 
-function show() {
-    for (const task of tasks) {
-        const li = document.createElement('li');
-        li.textContent = task;
-        ul.appendChild(li);
-    }
-    taskList.appendChild(ul);
+const div = [];
+const input = [];
+const label = [];
+
+function lastIndex(array){
+    return array[array.length-1];
 }
 
+function hoge() {
+    div.push(document.createElement('div'));
+    input.push(document.createElement('input'));
+    label.push(document.createElement('label'));
+}
+
+function hogeInfo() {
+    lastIndex(input).type = 'checkbox';
+    lastIndex(label).textContent = newTask.value;
+}
+
+function hogeAdd() {
+    lastIndex(div).appendChild(lastIndex(input));
+    lastIndex(div).appendChild(lastIndex(label));
+    taskList.insertBefore(lastIndex(div), taskList.firstElementChild);
+}
 
 const submit = document.getElementById('submit');
-const showList = document.getElementById('showList');
-
-submit.addEventListener('click', addTask);
-showList.addEventListener('click', show);
-*/
+submit.addEventListener('click', hoge);
+submit.addEventListener('click', hogeInfo);
+submit.addEventListener('click', hogeAdd);
