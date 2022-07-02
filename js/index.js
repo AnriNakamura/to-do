a1f32d133d48053ebd8b8097673b897ee92b52b4
AnriNakamura <shironeko1009@icloud.com>
2022-07-03 01:13:54 +0900

    add comments

--- js/index.js
+++ js/index.js
@@ -1,25 +1,27 @@
-const tasks = []; 
-
-// create new div, input(checkbox) and label
+// get input and list
 const newTask= document.getElementById('newTask');
 const taskList = document.getElementById('taskList');
 
+// element lists
 const div = [];
 const input = [];
 const label = [];
 
+// get last value of array
 function lastIndex(array){
     return array[array.length-1];
 }
 
+// add new task
 function addTask() {
+    // create element
     div.push(document.createElement('div'));
     input.push(document.createElement('input'));
     label.push(document.createElement('label'));
-
+    // add info of task
     lastIndex(input).type = 'checkbox';
     lastIndex(label).textContent = newTask.value;
-
+    // show task
     lastIndex(div).appendChild(lastIndex(input));
     lastIndex(div).appendChild(lastIndex(label));
     taskList.insertBefore(lastIndex(div), taskList.firstElementChild);
