52d57108c1f34793bd808017339a62c7520228f0
AnriNakamura <shironeko1009@icloud.com>
2022-07-09 13:40:37 +0900

    delete comment

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@ const taskList = document.getElementById('taskList');
 
 // element lists
 const div = [];
-const task = []; // list of things-to-do
-const input = []; // list of input contents
-const output = []; // list of output contents
+const task = [];    // list of things-to-do
+const input = [];   // list of input contents
+const output = [];  // list of output contents
 
 // get last value of array
 const lastIndex = (array) => array[array.length - 1];
@@ -15,26 +15,6 @@ const lastIndex = (array) => array[array.length - 1];
 newTask.focus();
 
 // add new task
-/*
-const input = [];
-const label = [];
-newTask.addEventListener('keydown', e => {
-    if (e.key === 'Enter') {
-        // create element
-        div.push(document.createElement('div'));
-        input.push(document.createElement('input'));
-        label.push(document.createElement('label'));
-        // add info of task
-        lastIndex(input).type = 'checkbox';
-        lastIndex(label).textContent = newTask.value;
-        // show task
-        lastIndex(div).appendChild(lastIndex(input));
-        lastIndex(div).appendChild(lastIndex(label));
-        taskList.insertBefore(lastIndex(div), taskList.firstElementChild);
-    }
-});
-*/
-
 newTask.addEventListener('keydown', e => {
     if (e.key === 'Enter') {
         // create element
