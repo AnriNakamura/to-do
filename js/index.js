2e74a4b715f995dd2de1ace691f48c67dc09d790
AnriNakamura <shironeko1009@icloud.com>
2022-07-09 10:40:40 +0900

    add event of keydown

--- js/index.js
+++ js/index.js
@@ -11,17 +11,18 @@ const label = [];
 const lastIndex = (array) => array[array.length-1];
 
 // add new task
-const submit = document.getElementById('submit');
-submit.addEventListener('click', () => {
-    // create element
-    div.push(document.createElement('div'));
-    input.push(document.createElement('input'));
-    label.push(document.createElement('label'));
-    // add info of task
-    lastIndex(input).type = 'checkbox';
-    lastIndex(label).textContent = newTask.value;
-    // show task
-    lastIndex(div).appendChild(lastIndex(input));
-    lastIndex(div).appendChild(lastIndex(label));
-    taskList.insertBefore(lastIndex(div), taskList.firstElementChild);
+newTask.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+        // create element
+        div.push(document.createElement('div'));
+        input.push(document.createElement('input'));
+        label.push(document.createElement('label'));
+        // add info of task
+        lastIndex(input).type = 'checkbox';
+        lastIndex(label).textContent = newTask.value;
+        // show task
+        lastIndex(div).appendChild(lastIndex(input));
+        lastIndex(div).appendChild(lastIndex(label));
+        taskList.insertBefore(lastIndex(div), taskList.firstElementChild);
+    }
 });
